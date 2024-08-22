f5dff9f7c157e5bcfce892d1924697f5a8f0855e
poriteku <etrobo.poritekuhm@gmail.com>
2024-08-22 17:44:02 +0900

    シーン番号②における直進走行のエラー対策

--- Detect/RunDistDetect.cpp
+++ Detect/RunDistDetect.cpp
@@ -1,4 +1,5 @@
 #include "RunDistDetect.h"
+#include <stdio.h>
 
 using namespace ev3api;
 
@@ -10,6 +11,9 @@ RunDistDetect::RunDistDetect(unsigned char tire)
 {
     LeftMotorEncoder.setCount(0);
     RightMotorEncoder.setCount(0);
+    printf("RunDistDetectクラスのコンストラクタ\n");
+    printf("mold_left_cnt : %d\n", mold_left_cnt);
+    printf("mold_right_cnt : %d\n", mold_right_cnt);
 }
 
 RunDistDetect::~RunDistDetect()
@@ -20,6 +24,7 @@ RunDistDetect::~RunDistDetect()
 float RunDistDetect::getRunDist()
 {
     measureRunDist();
+    printf("RunDistDetect::getRunDist関数におけるmrun_distの値：%f\n", mrun_dist);
     return mrun_dist;
 }
 
@@ -43,15 +48,23 @@ void RunDistDetect::measureRunDist()
     }
     else if (mtire == BOTH)
     {
+        printf("mtire is BOTH\n");
         mright_cnt = RightMotorEncoder.getCount();
         mleft_cnt = LeftMotorEncoder.getCount();
+        printf("mright_cnt : %d\n", mright_cnt);
+        printf("mleft_cnt  : %d\n", mleft_cnt);
 
         mright_sum += mright_cnt - mold_right_cnt;
         mleft_sum += mleft_cnt - mold_left_cnt;
+        printf("mright_sum : %f\n", mright_sum);
+        printf("mleft_sum  : %f\n", mleft_sum);
 
         mrun_dist = (mright_sum + mleft_sum) * mANGLE1DIST / 2.0;
+        printf("mrun_dist : %f\n", mrun_dist);
 
         mold_right_cnt = mright_cnt;
         mold_left_cnt = mleft_cnt;
+        printf("mright_old_cnt : %d\n", mright_old_cnt);
+        printf("mleft_old_cnt  : %d\n", mleft_old_cnt);
     }
 }
